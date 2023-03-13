d8043e43cddad35bfc99f8650fd1621cdaae8501
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-03-13 17:28:28 +0100

    Added basic firebase auth functionality

--- app/src/main/java/com/example/cardhub/LoginActivity.java
+++ app/src/main/java/com/example/cardhub/LoginActivity.java
@@ -1,35 +1,96 @@
 package com.example.cardhub;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
         // Instantiate layout components
-        EditText et_username = findViewById(R.id.editTextText_username);
+        EditText et_email = findViewById(R.id.editText_email);
         EditText et_password = findViewById(R.id.editText_password);
         TextView tv_forgot = findViewById(R.id.textView_forgot);
         TextView tv_register = findViewById(R.id.textView_register);
         Button btn_login = findViewById(R.id.button_login);
+
+        // Login Button is Pressed
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
 
-                // Ensure no returns to login screen
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                // Instantiate variables
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(LoginActivity.this, "Authentication succeeded.",
+                                            Toast.LENGTH_SHORT).show();
+                                    // Move to next screen
+                                    Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+
+                                    // Ensure no returns to login screen
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed. " + password,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Toast.makeText(LoginActivity.this, "Still logged in!",
+                    Toast.LENGTH_SHORT).show();
+
+            // Move to next screen
+            Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+
+            // Ensure no returns to login screen
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/cardhub/MapActivity.java
+++ app/src/main/java/com/example/cardhub/MapActivity.java
@@ -2,7 +2,13 @@ package com.example.cardhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        // Instantiate layout components
+        Button btn_logout = findViewById(R.id.button_logout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Log user out
+                FirebaseAuth.getInstance().signOut();
+
+                // Show toast
+                Toast.makeText(MapActivity.this, "Logged out!",
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+                // Ensure no returns to login screen
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
