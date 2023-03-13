c11a0518d600259562e39216e1fefd5f356c871d
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-03-13 16:31:07 +0100

    Added basic navigation flag rules

--- app/src/main/java/com/example/cardhub/LoginActivity.java
+++ app/src/main/java/com/example/cardhub/LoginActivity.java
@@ -2,13 +2,34 @@ package com.example.cardhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Instantiate layout components
+        EditText et_username = findViewById(R.id.editTextText_username);
+        EditText et_password = findViewById(R.id.editText_password);
+        TextView tv_forgot = findViewById(R.id.textView_forgot);
+        TextView tv_register = findViewById(R.id.textView_register);
+        Button btn_login = findViewById(R.id.button_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+
+                // Ensure no returns to login screen
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
