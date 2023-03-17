56074e304073589403b94a6e9e6d35c0ceca7146
ETKuppens <et.kuppens@gmail.com>
2023-03-17 14:16:28 +0100

    Add TradingModeState class
    Moved the progress of the last commit to this new TradingModeState class.

--- app/src/main/java/com/example/cardhub/TradeModeActivity.java
+++ app/src/main/java/com/example/cardhub/TradeModeActivity.java
@@ -6,19 +6,9 @@ import android.os.Bundle;
 
 public class TradeModeActivity extends AppCompatActivity {
 
-    // TradingSession instance that keeps track which user proposes which cards in the current
-    // trading session.
-    TradingSession tradingSession = new TradingSession();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade_mode);
     }
-
-    /**
-     * Update the UI using the data from the TradingSession instance.
-     * TODO: implement this method
-     */
-    private void updateUI() {}
 }
