08afdf53c183dac82fdc822e19576e73241bf042
ETKuppens <et.kuppens@gmail.com>
2023-03-22 17:03:18 +0100

    Listen to the Ready and Cancel buttons

--- app/src/main/java/com/example/cardhub/TradeModeActivity.java
+++ app/src/main/java/com/example/cardhub/TradeModeActivity.java
@@ -3,15 +3,26 @@ package com.example.cardhub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TradeModeActivity extends AppCompatActivity {
+public class TradeModeActivity extends AppCompatActivity implements View.OnClickListener {
 
     TradeModeState state;
+
+    Button readyButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trade_mode);
+
+        readyButton = findViewById(R.id.ReadyButton);
+        cancelButton = findViewById(R.id.CancelButton);
+
+        readyButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
     }
 
     /**
@@ -30,5 +41,31 @@ public class TradeModeActivity extends AppCompatActivity {
 
     public void enableChangeTradeProposal() {
 
+    }
+
+
+    private void cancelButtonClicked() {
+
+    }
+
+    private void readyButtonClicked() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Change functionality depending on which button was clicked.
+        switch(view.getId()) {
+            case R.id.CancelButton:
+                cancelButtonClicked();
+                break;
+
+            case R.id.ReadyButton:
+                readyButtonClicked();
+                break;
+
+            default:
+                break;
+        }
     }
 }
