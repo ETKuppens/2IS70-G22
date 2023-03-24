fc767ea0aafdddb1671b3172c5d9fe82ceada331
ETKuppens <et.kuppens@gmail.com>
2023-03-24 15:24:21 +0100

    Move setResult to its own function

--- app/src/main/java/com/example/cardhub/inventory/InventoryActivity.java
+++ app/src/main/java/com/example/cardhub/inventory/InventoryActivity.java
@@ -18,33 +18,43 @@ public class InventoryActivity extends AppCompatActivity {
     InventoryState state;
 
     boolean shouldSupportChoosingACard = false;
+    CardDiff diff = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Intent intent = getIntent();
-        Bundle intentBundle = intent.getExtras();
-
-        if (intentBundle != null) {
-            String intentOrigin = intentBundle.getString("origin");
-
-            if (intentOrigin != null && intentOrigin.equals("TradeModeActivity")) {
-                this.shouldSupportChoosingACard = true;
-            }
-        }
-
-        CardDiff diff = null;
-        Gson converter = new Gson();
-        String convertedCardDiff = converter.toJson(diff);
-        intent.putExtra("CardDiff", convertedCardDiff);
-        setResult(RESULT_OK, intent);
-        finish();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
 
         state = new InventoryState(this);
 
         state.requestCards();
+    }
+
+    /**
+     * Stop this activity by using {@code finish()}.
+     * If this InventoryActivity instance is launched with the purpose of choosing a card from the
+     * inventory and a card has been chosen, set that card as a result.
+     */
+    public void stopActivity() {
+        if (shouldSupportChoosingACard && diff != null) {
+            Intent intent = getIntent();
+            Bundle intentBundle = intent.getExtras();
+
+            if (intentBundle != null) {
+                String intentOrigin = intentBundle.getString("origin");
+
+                if (intentOrigin != null && intentOrigin.equals("TradeModeActivity")) {
+                    this.shouldSupportChoosingACard = true;
+                }
+            }
+
+            Gson converter = new Gson();
+            String convertedCardDiff = converter.toJson(diff);
+            intent.putExtra("CardDiff", convertedCardDiff);
+            setResult(RESULT_OK, intent);
+        }
+
+        finish();
     }
 
     /**
