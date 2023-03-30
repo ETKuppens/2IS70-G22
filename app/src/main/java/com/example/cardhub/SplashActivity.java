c7005691bd13f8aae02e08a7b6a2c9bd91669bfa
Rijkman <rijkmanpilaar@gmail.com>
2023-03-30 14:36:37 +0200

    Added new color scheme

---
+++ app/src/main/java/com/example/cardhub/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.cardhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //getActionBar().hide();
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },10000);
+    }
+}
