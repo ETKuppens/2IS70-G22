99ae687d0849f262a4731c96d3ac1103deb08167
Tulgar Dinç <tulgardinc@gmail.com>
2023-04-05 01:19:07 +0200

    initial commit

--- app/src/main/java/com/example/cardhub/card_creation/CardCreationData.java
+++ app/src/main/java/com/example/cardhub/card_creation/CardCreationData.java
@@ -1,12 +1,21 @@
 package com.example.cardhub.card_creation;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.cardhub.inventory.Card;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 
 public class CardCreationData {
     FirebaseFirestore db;
+    FirebaseAuth
 
     CardCreationData() {
         this.db = FirebaseFirestore.getInstance();
@@ -19,6 +28,15 @@ public class CardCreationData {
         cardObj.put("rarity", c.RARITY);
         cardObj.put("imageurl", c.IMAGE_URL);
 
-        db.collection("cards/").add(cardObj);
+        db.collection("cards/").add(cardObj).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                if (task.isSuccessful()) {
+                    db.collection("")
+                } else {
+                    Log.d("CARD_CREATION", "failed to add card to card pool");
+                }
+            }
+        });
     }
 }
