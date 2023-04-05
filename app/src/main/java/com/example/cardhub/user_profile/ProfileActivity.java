8615e743b1dff612e07547d552f159d7bf15b555
ETKuppens <et.kuppens@gmail.com>
2023-04-05 14:46:13 +0200

    Add cardpack preview as popup window (does not show cardpack yet)

--- app/src/main/java/com/example/cardhub/user_profile/ProfileActivity.java
+++ app/src/main/java/com/example/cardhub/user_profile/ProfileActivity.java
@@ -1,14 +1,21 @@
 package com.example.cardhub.user_profile;
 
 import android.content.Intent;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.os.Bundle;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.cardhub.map.MapActivity;
 import com.example.cardhub.PairingModeActivity;
@@ -20,6 +27,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends CollectorBaseActivity {
     ProfileState state;
+
+    PopupWindow cardpackPreviewWindow = null;
 
     @Override
     public int getLayoutId() {
@@ -48,6 +57,35 @@ public class ProfileActivity extends CollectorBaseActivity {
                 Intent logout = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(logout);
                 FirebaseAuth.getInstance().signOut();
+            }
+        });
+
+        Button cardpackPreviewTestButton = (Button)findViewById(R.id.test_cardpack_preview_button);
+        cardpackPreviewTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardpackPreviewWindow != null) {
+                    return;
+                }
+
+                LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+                View cardpackPreviewView = inflater.inflate(R.layout.cardpack_preview, null);
+
+                cardpackPreviewWindow = new PopupWindow(cardpackPreviewView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+
+                ImageButton cardpackPreviewCloseButton = cardpackPreviewView
+                        .findViewById(R.id.cardpack_preview_close_button);
+
+                cardpackPreviewCloseButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        destroyCardpackPreviewWindow();
+                    }
+                });
+
+                cardpackPreviewWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
             }
         });
     }
@@ -80,5 +118,15 @@ public class ProfileActivity extends CollectorBaseActivity {
         tradeAmount.setText(state.getTradeAmount());
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
+        destroyCardpackPreviewWindow();
+    }
+
+    private void destroyCardpackPreviewWindow() {
+        cardpackPreviewWindow.dismiss();
+        cardpackPreviewWindow = null;
+    }
 }
