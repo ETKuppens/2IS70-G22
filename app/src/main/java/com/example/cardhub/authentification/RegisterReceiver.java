e82dc7fa307b8c91f3003749522a9a6b20d03068
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-04-16 08:28:57 +0200

    Added Javadoc comments

--- app/src/main/java/com/example/cardhub/authentification/RegisterReceiver.java
+++ app/src/main/java/com/example/cardhub/authentification/RegisterReceiver.java
@@ -3,9 +3,27 @@ package com.example.cardhub.authentification;
 import com.google.firebase.auth.FirebaseUser;
 
 /**
- * Interface
+ * Interface for the State Design pattern implementation of the registration of
+ * users.
+ *
+ * @author  Vladislav Budiak, Sevket Tulgar Dinc, Etienne Kuppens,
+ *          Aqiel Oostenbrug, Marios Papalouka, Rijkman Pilaar
+ * @groupname Group 22
+ * @date 16-04-2023
  */
 public interface RegisterReceiver {
+    /**
+     * Takes appropriate action upon the successful registration of the
+     * {@code user}.
+     *
+     * @param user user that has been successfully registered
+     * @param role role of the the user that has been successfully registered
+     */
     public void registrationSuccess(FirebaseUser user, String role);
+
+    /**
+     * Takes appropriate action upon the unsuccessful registration of the
+     * {@code user}.
+     */
     public void registrationFail();
 }
