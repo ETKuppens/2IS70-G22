8ee24a8b43ea280692d3523c97bd3f869549112d
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-04-16 21:24:14 +0200

    Created test file

---
+++ app/src/test/java/com/example/cardhub/authentication/ForgotPasswordStateTest.java
@@ -0,0 +1,34 @@
+package com.example.cardhub.authentication;
+
+import junit.framework.TestCase;
+
+public class ForgotPasswordStateTest extends TestCase {
+//    private ForgotPasswordState state;
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Variables
+//        ForgotPasswordActivity activity = new ForgotPasswordActivity();
+//        this.state = new ForgotPasswordState(activity);
+    }
+
+    public void testSendForgotPasswordEmailSuccess() {
+//        ForgotPasswordActivity activity = mock()
+//        ForgotPasswordActivity activity = new ForgotPasswordActivity() {
+//            public sendForgotPasswordEmailSuccess() {
+//                return true;
+//            }
+//        };
+//        state.sendForgotPasswordEmailSuccess();
+    }
+
+    public void testSendForgotPasswordEmailFailure() {
+//        state.sendForgotPasswordEmailFailure();
+    }
+
+    public void testSendForgotPasswordEmail() {
+//        String email = "";
+//        state.sendForgotPasswordEmail(email);
+    }
+}
