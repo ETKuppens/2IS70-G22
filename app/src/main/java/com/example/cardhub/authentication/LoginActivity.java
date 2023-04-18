2d28a0b465be04b8cf1195bd2a909ba1c5530f9d
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-04-18 13:50:49 +0200

    Adding lambda's

--- app/src/main/java/com/example/cardhub/authentication/LoginActivity.java
+++ app/src/main/java/com/example/cardhub/authentication/LoginActivity.java
@@ -2,12 +2,13 @@ package com.example.cardhub.authentication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.cardhub.R;
 
 /**
@@ -30,33 +31,24 @@ public class LoginActivity extends AppCompatActivity {
         Button btn_login = findViewById(R.id.button_login);
 
         // Login button is pressed
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Instantiate variables
-                String email = et_email.getText().toString();
-                String password = et_password.getText().toString();
+        btn_login.setOnClickListener(view -> {
+            // Instantiate variables
+            String email = et_email.getText().toString();
+            String password = et_password.getText().toString();
 
-                state.signIn(email, password); // Pass authentication request
-            }
+            state.signIn(email, password); // Pass authentication request
         });
 
         // Registration link is pressed
-        tv_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Open RegistrationActivity
-                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
-            }
+        tv_register.setOnClickListener(view -> {
+            // Open RegistrationActivity
+            startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
         });
 
         // Forgotten password link is pressed
-        tv_forgot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Open ForgotPasswordActivity
-                startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class));
-            }
+        tv_forgot.setOnClickListener(view -> {
+            // Open ForgotPasswordActivity
+            startActivity(new Intent(getApplicationContext(), ForgotPasswordActivity.class));
         });
     }
 
