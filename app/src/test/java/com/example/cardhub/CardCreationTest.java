dae4b8187217c4fa00302a5213b83960c16bce70
Rijkman <rijkmanpilaar@gmail.com>
2023-04-18 12:38:32 +0200

    initial commit

---
+++ app/src/test/java/com/example/cardhub/CardCreationTest.java
@@ -0,0 +1,12 @@
+package com.example.cardhub;
+
+import com.example.cardhub.card_creation.CardCreationState;
+
+public class CardCreationTest {
+    CardCreationState testState = new CardCreationState();
+
+
+    public void testEmptyName() {
+        testState.
+    }
+}
