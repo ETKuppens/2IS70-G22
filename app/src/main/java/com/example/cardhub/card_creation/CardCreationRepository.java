4e5ebac66816ef9e50ec0ced2518165b16778a8b
Rijkman <rijkmanpilaar@gmail.com>
2023-04-19 13:47:14 +0200

    Added all tests

--- app/src/main/java/com/example/cardhub/card_creation/CardCreationRepository.java
+++ app/src/main/java/com/example/cardhub/card_creation/CardCreationRepository.java
@@ -1,26 +1,2 @@
-package com.example.cardhub.card_creation;
-
-import com.example.cardhub.inventory.Card;
-
-/**
- * Repository Class for creating cards.
- * @author Rijkman
- */
-public class CardCreationRepository {
-    //Database instance
-    CardCreationData data;
-    //Constructor
-    CardCreationRepository() {
-        data = new CardCreationData();
-    }
-
-
-    /**
-     * Publishes a given card to the database pool.
-     * @pre The card is valid
-     * @param c The card that needs to be published
-     */
-    public void publishCard(Card c) {
-        data.publishCard(c);
-    }
+package com.example.cardhub.card_creation;public interface CardCreationRepository {
 }
