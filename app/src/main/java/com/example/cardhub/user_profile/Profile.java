32c5a36209c379c2a4d53fa4fe5d5f8a5ddc5455
Rijkman <rijkmanpilaar@gmail.com>
2023-04-20 15:45:19 +0200

    added profile class tests

--- app/src/main/java/com/example/cardhub/user_profile/Profile.java
+++ app/src/main/java/com/example/cardhub/user_profile/Profile.java
@@ -8,12 +8,18 @@ import com.example.cardhub.inventory.Card;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Data structure that represents a user profile
+ * @author Rijkman and Tulgar
+ */
 public class Profile {
+    //Stores the profiles Username
     private String userName;
-
+    //Users profile picture
     private ImageView profilePicture;
-
+    //Amount of cards collected
     private int cardsCollected;
+    //Amount of trades made
     private int tradesMade;
 
     public Profile(ImageView profilePicture, String userName, int cardsCollected, int tradesMade) {
@@ -31,23 +37,46 @@ public class Profile {
         this.tradesMade = (int)  data.get("tradesmade");
     }
 
+    /**
+     * Changes the current profile picture to a new one.
+     * @param profilePicture the profile picture to replace it with
+     * @throws NullPointerException if profilePicture == null
+     */
     public void setProfilePicture(ImageView profilePicture) {
+        if (profilePicture == null) {
+            throw new NullPointerException();
+        }
         this.profilePicture = profilePicture;
     }
 
+    /**
+     * Returns the current profile picture.
+     * @return the picture as an ImageView object
+     */
     public ImageView getProfilePicture() {
         return profilePicture;
     }
 
+    /**
+     * Returns the amount of cards this user has.
+     * @return the amount of cards as a string object
+     */
     public String getCardAmount() {
         return String.valueOf(this.cardsCollected);
     }
 
+    /**
+     * Returns the amount of trades made by the current user.
+     * @return the amount of trades a String object
+     */
     public String getTradeAmount() {
         return String.valueOf(this.tradesMade);
     }
 
-
+    /**
+     * Returns the username of the current user.
+     * @return the username as string object
+     */
     public String getUsername() {
         Log.d("PROFILE3", userName);
         return String.valueOf(this.userName);
---
+++ app/src/test/java/com/example/cardhub/user_profile/ProfileStateTest.java
@@ -0,0 +1,13 @@
+package com.example.cardhub.user_profile;
+
+import android.widget.ImageView;
+
+import org.junit.Test;
+
+/**
+ * Tests for the Profile are exclusively done in the DataTests, since that is where the
+ * actual functionality of the Profile is. The State is really just an intermediary.
+ */
+public class ProfileStateTest {
+
+}
