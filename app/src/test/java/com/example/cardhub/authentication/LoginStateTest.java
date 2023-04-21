c566a497928db5ccb21ffe2dc17094047a6572b7
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2023-04-21 13:07:24 +0200

    Added null constructor tests

--- app/src/test/java/com/example/cardhub/authentication/LoginStateTest.java
+++ app/src/test/java/com/example/cardhub/authentication/LoginStateTest.java
@@ -1,13 +1,60 @@
 package com.example.cardhub.authentication;
 
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+/**
+ * Tests LoginState.
+ *
+ * @author  Vladislav Budiak, Sevket Tulgar Dinc, Etienne Kuppens,
+ *          Aqiel Oostenbrug, Marios Papalouka, Rijkman Pilaar
+ * @groupname Group 22
+ * @date 21-04-2023
+ */
 public class LoginStateTest {
+    // Variables
+    @Mock
+    private LoginActivity activity;
+    @Mock
+    private LoginData data;
+    @Mock
+    private final LoginState state;
+
+    public LoginStateTest() {
+        MockitoAnnotations.openMocks(this);
+        this.state = new LoginState(activity, data);
+    }
+
     /**
      * Test that {@code LoginState} propagates a NullPointerException when {@code activity == null}.
      */
     @Test(expected = NullPointerException.class)
-    public void testLoginStateNull() {
+    public void testLoginStateNullException0() {
         new LoginState(null);
+    }
+
+    /**
+     * Test that {@code LoginState} propagates a NullPointerException when {@code activity == null && data == null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testLoginStateNullException1() {
+        new LoginState(null, null);
+    }
+
+    /**
+     * Test that {@code LoginState} propagates a NullPointerException when {@code activity != null && data == null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testLoginStateNullException2() {
+        new LoginState(activity, null);
+    }
+
+    /**
+     * Test that {@code LoginState} propagates a NullPointerException when {@code activity == null && data != null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testLoginStateNullException3() {
+        new LoginState(null, data);
     }
 }
